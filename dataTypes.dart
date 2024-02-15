1d99f810546c5dc84e5f0aece0edfbbcada1c0f8
salma <salmaelmaghawry91@gmail.com>
2024-02-15 06:33:30 +0200

    general data types defenition

--- dataTypes.dart
+++ dataTypes.dart
@@ -1,4 +1,19 @@
 void main() {
+  /*
+  =>primitive data types 
+  String 
+  int 
+  double 
+  boolen 
+  dynamic 
+  
+  =>collection data types 
+
+   */
   String name = "salma";
-  print("name is :$name") ;
+  print("name is :$name");
+  name = "elmaghawry";
+  
+
+
 }
