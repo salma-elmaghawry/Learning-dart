04d09e859082f2f05ec03e0833f3d008bee48dc1
salma <salmaelmaghawry91@gmail.com>
2024-02-15 06:19:39 +0200

    Variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
-//main function
-void main()
-{
-  print("salma elmaghawry");
+//variables part 1
+void main() {
+  /*
+  لما بننشا متغير يعنى احنا حجزنا مكان فى الميمورى 
+  نحدد نوع البيانات اللى بتتغير data type 
+  اسم الحاجه اللى بتتغير name of variable 
+  text in programming=>string
+  String=> لازم ال لاس تكون كابتل 
+    = assign operator  
+  مينغعش نبدا اسم المتغير بارقام 
+  $ =>to concatenate string with variable 
+
+
   
+   */
+  String Name="salma "; //كده انشانا مكان فى الميمورى
+  //Name = "'Salma Elmaghawry'"; //كده حطينا قيمه
+  //memory[salma]=>Name
+  print("my name is $Name");
 }
