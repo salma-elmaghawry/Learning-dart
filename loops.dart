302129137d5152533aeed90dccb3cb138c46dcc3
salma <salmaelmaghawry91@gmail.com>
2024-02-23 18:27:01 +0200

    while loop

--- loops.dart
+++ loops.dart
@@ -1,13 +1,23 @@
 void main() {
   List<String> names = ['salma', 'mohamed', 'elmaghawry'];
-  
+
   //for loop
   //do while loop
   //while loop
   //for in
   //for each
-  for (int i = 0; i<names.length; i++) {
+  //=============for loop ================//
+  for (int i = 0; i < names.length; i++) {
     print(names[i]);
   }
+  //===============while loop===================//
+  int x = 5;
+  bool isRunning = false;
+  while (isRunning) {
+    print("salma elmaghawry");
+  }
 
+  //==================do while loop ====================//
+  
+  
 }
