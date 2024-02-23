a81adf1fd9190582867f37413316fc2c87b7be4a
salma <salmaelmaghawry91@gmail.com>
2024-02-23 17:46:22 +0200

    for loop

---
+++ loops.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<String> names = ['salma', 'mohamed', 'elmaghawry'];
+  
+  //for loop
+  //do while loop
+  //while loop
+  //for in
+  //for each
+  for (int i = 0; i<names.length; i++) {
+    print(names[i]);
+  }
+
+}
