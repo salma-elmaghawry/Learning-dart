3fa124f88dd391d19da629a08f10260753106cd6
salma <salmaelmaghawry91@gmail.com>
2024-03-09 01:34:07 +0200

    for in loop

--- forInLoop.dart
+++ forInLoop.dart
@@ -1,4 +1,12 @@
-void main()
-{
+void main() {
+  //for in loop//
+  List<String> names = ["salma", "Ahmed", "moahmed "];
+  //لكل ايتيم موجو فى ال list 
+  //[]=>
+  //مشكلتها ان مفيش index 
+  for (var item in names)
+   {
+    print(item);
+  }
   
 }
