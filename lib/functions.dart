2feeb86ee9c13a7995521b742b358fd468e9cc5f
salma <salmaelmaghawry91@gmail.com>
2024-03-12 16:36:33 +0200

    Functions

---
+++ lib/functions.dart
@@ -0,0 +1,15 @@
+void main() { //main function 
+  printMyStory();
+  print("After calling this function");
+}
+//ال لوب بتكرر ولا بعض
+//الفانكش بتكرر بمزاجى المره اللى انا بحددها
+
+//synatx
+void printMyStory() {
+  print("salma  elmaghaery ");
+  print("alkefafy");
+  print("i love this");
+}
+//print is also a function =>bultin function 
+//ودول بيكونوا جايين من ال dart sdk 
