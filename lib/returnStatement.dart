8146b6dbf3f4c441a170e0bd9fb178c39f16f5a6
salma <salmaelmaghawry91@gmail.com>
2024-03-12 16:51:20 +0200

    Return function

---
+++ lib/returnStatement.dart
@@ -0,0 +1,28 @@
+void main() {
+  int result = sumTwoNumbers();
+  print(result + 4);
+}
+
+int sumTwoNumbers() {
+  int number1 = 15;
+  int number2 = 40;
+  int result = number1 + number2;
+  print(result);
+  return result;
+}
+/*
+لو انا هعمل return 
+يبقى لازم احدد الداتا تايب 
+int=>return data type 
+void=> مش بترجع حاجه 
+  int result = sumTwoNumbers();
+  لازم هنا ال result تكون نفس الداتا تاييب بتاع الفانكشن
+  
+
+
+
+
+
+
+
+ */
