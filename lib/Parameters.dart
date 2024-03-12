11b1bf28fa5aa54589e98691c8a3167b3a92b1b6
salma <salmaelmaghawry91@gmail.com>
2024-03-12 16:57:38 +0200

    Parameters

---
+++ lib/Parameters.dart
@@ -0,0 +1,10 @@
+void main() {
+  int result = sumTwoNumbers(3, 7, 3);
+  int result2 = sumTwoNumbers(5, 4, 3);
+}
+
+int sumTwoNumbers(int num1, int num2, int num3) {
+  int result = num1 + num2 + num3;
+  print(result);
+  return result;
+}
